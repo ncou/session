c84e6b9ab728b9f3474a57da9d29d959b9ccd300
ncou <nicolas_courade@yahoo.fr>
2021-01-31 16:54:30 +0100

    update

--- src/Bootloader/PublishSessionBootloader.php
+++ src/Bootloader/PublishSessionBootloader.php
@@ -6,13 +6,13 @@ namespace Chiron\Session\Bootloader;
 
 use Chiron\Core\Container\Bootloader\AbstractBootloader;
 use Chiron\Core\Directories;
-use Chiron\PublishableCollection;
+use Chiron\Core\Publisher;
 
 final class PublishSessionBootloader extends AbstractBootloader
 {
-    public function boot(PublishableCollection $publishable, Directories $directories): void
+    public function boot(Publisher $publisher, Directories $directories): void
     {
         // copy the configuration file template from the package "config" folder to the user "config" folder.
-        $publishable->add(__DIR__ . '/../../config/session.php.dist', $directories->get('@config/session.php'));
+        $publisher->add(__DIR__ . '/../../config/session.php.dist', $directories->get('@config/session.php'));
     }
 }
